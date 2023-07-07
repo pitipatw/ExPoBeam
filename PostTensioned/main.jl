a13caa99c60c0978b75b3f8599e83e230e4ae399
Pitipat-PC <pitipatw@mit.edu>
2023-07-07 15:57:51 -0400

    Added constraints

--- PostTensioned/main.jl
+++ PostTensioned/main.jl
@@ -11,11 +11,18 @@ with Centroid as a function of "c" -> Excel
 
 get ARM of the section.
 """
+"""
+Notes
+Shaer strength depends on the current applied axial force
+Therefore, shear will depend on the axial and will decresase if the applied axial force decreases.
+"""
+
 
 using Printf
 
 #Concrete information
 fc′ = 133.7
+ftension = 0.17*sqrt(fc′)
 ρc = 2400.0 # concrete density
 Ec = 4700.0*fc′^0.5 # concrete modulus of elasticity
 
@@ -30,7 +37,7 @@ ep = 200_000.0 #[MPa]
 need work
 """
 #PixelFrame Section properties
-ap = 1000.0 #Total concrete area
+ac = 1000.0 #Total concrete area
 shear_ratio = 0.3 #ratio of the section that resists shear to the total area.
 d = 500.0 #beamdepth
 #Serviceability parameters
@@ -60,9 +67,11 @@ mtotal = w_tot*l^2/8.0 #[Nmm]
 ccn = 0.85*fc′*ap
 pn = (ccn - (fpe - 0.003*ep)*aps) / 1000 #[kN]
 pu = 0.65*0.8*pn #[kN]
+ptforce = pu #[kN]
 @printf "The pure compression capacity is %.3f [kN]\n" pu
 
 #Pure Moment Capacity
+fps  =Fps
 acomp = aps*fps/(0.85*fc′)
 arm = d - c
 mn_steel = aps*fps*arm/1e6
@@ -70,17 +79,50 @@ mn_steel = aps*fps*arm/1e6
 mn_conc = 0.0 #***NEED WORK***
 #check if mn_conc is possible.
 
-Mu = Φ(ϵ)*mn_steel #[kNmm]
+mu = Φ(ϵ)*mn_steel #[kNmm]
 
-@printf "The pure moment capacity is %.3f [kNmm]\n" Mu
+@printf "The pure moment capacity is %.3f [kNmm]\n" mu
 
 #Shear Calculation
 ashear = ap*shear_ratio
+fctk = ftension
 ρs = aps/ashear
 k = clamp(sqrt(200.0/d),0,2.)
 fFts = 0.45*fR1
 wu = 1.5
 CMOD3 = 1.5
-NEd = ptforce
-σcp = 
-fFtu = Get_fFtu(fFts, wu, CMOD3, fR1, fR3)
+ned = ptforce
+σcp1 = ned/ac
+σcp2 = 0.2*fc′
+σcp = clamp(σcp1, 0.0, σcp2)
+fFtu = get_fFtu(fFts, wu, CMOD3, fR1, fR3)
+vn = ashear*get_v(ρs, fc′,fctk, fFtu, 1.0, σcp1, k) #kN
+vu = 0.75*vn 
+
+@printf "The shear capacity is %.3f [kN]\n" vu
+
+
+#Constraint check
+#Deflection limit
+#get from the march test model.
+
+δmid = 0.5 #(will have to work on this)
+
+δlimit = l/240.0 #[mm]
+
+#first constraint, deflection limit (1)
+c1 = δlimit > δmid 
+
+#second constraint, stress limit
+#Initial (Post tensioning) stage (2)
+#tensionlimit
+ftopinit = -pi/ac + pi*e/st - mdead/st
+c2t = ftopinit <= somenuber
+#compression limit
+fbotinit = -pi/ac - pi*e/st + mdead/sb
+c2c = fbotinit <= somenumber
+
+#Service stage (3)
+#compression limit
+ftopservice = -pi/ac + pi*e/st - mt/st
+c3 = 
--- PostTensioned/ptFunc.jl
+++ PostTensioned/ptFunc.jl
@@ -17,12 +17,12 @@ end
 """
 fpy is based on ASTM A421
 """
-function Fps(fpe::Float64, fc′::Float64,ρₚ::Float64; fpy::Float64 = 1300.0)
+function get_fps(fpe::Float64, fc′::Float64,ρₚ::Float64; fpy::Float64 = 1300.0)
     out = min(fpe + 70.0 + fc′/(100.0*ρₚ), fpe + 420.0, fpy)
     return out
 end
 
-function Get_fFtu(fFts::Float64, wu::Float64, CMOD3::Float64,fR1::Float64, fR3::Float64)
+function get_fFtu(fFts::Float64, wu::Float64, CMOD3::Float64,fR1::Float64, fR3::Float64)
     out = fFts - wu/CMOD3*(fFts-0.5*fR3 + 0.2*fR1)
     return out 
 end
@@ -30,6 +30,7 @@ end
 fFtuk = fFtu
 fck = fc′
 """
-function V(ρs::Float64, fck::Float64, fFtuk::Float64, γc::Float64, scp::Float64, k::Float64)
+function get_v(ρs::Float64, fck::Float64, fFtuk::Float64, γc::Float64, scp::Float64, k::Float64)
     out = 0.18/γc*k*(100.0*ρs *(1.0 + 7.5*fFtuk/fctk)* fck)^(1/3)+0.15*scp
 return out/1000.
+end
