880889c8de75bf139ecabc436d24a1802ca91a32
Pitipat-PC <pitipatw@mit.edu>
2023-07-08 02:29:22 -0400

    Numerical method for inertia

---
+++ PostTensioned/#integrate from points.jl
@@ -0,0 +1,61 @@
+#integrate from points
+using PolygonInbounds
+nodes = [0. 0.; 0. 10.; 10.0 10.0 ;10. 0.;]
+A = 1/2*10*10
+x = 0.:0.01:10.
+y = 0.:0.01:10.
+points = Matrix{Float64}(undef, size(x)[1]*size(y)[1], 2)
+for i =1:size(x)[1]
+    for j = 1:size(y)[1]
+        points[(i-1)*size(y)[1]+j,:] = [x[i], y[j]]
+    end
+end
+edges = Matrix{Int64}(undef, size(nodes)[1], 2)
+for i = 1:size(nodes)[1]
+    if i != size(nodes)[1]
+        edges[i,:] =  [i, i+1]
+    else 
+        edges[i,:] =  [i, 1]
+    end
+end
+edges #must be int!
+tol = 1e-1
+
+stat = inpoly2(points, nodes, edges, atol =tol)
+
+# f2 = Figure(resolution = (800, 600))
+# ax2 = Axis(f2[1, 1], xlabel = "x", ylabel = "y", aspect = DataAspect())#, aspect = DataAspect(), xgrid = false, ygrid = false)
+# # p2 = scatter!(ax2, ptx,pty, color = :red )
+# @time for i = 1:size(points)[1]
+#     if stat[i,1] == true
+#         scatter!(ax2, points[i,1], points[i,2], color = :green, markersize = 10)
+#     else
+#         scatter!(ax2, points[i,1], points[i,2], color = :blue, markersize = 2)
+#     end
+# end
+# f2
+
+
+inertia = 0.0
+p_inpoly = points[stat[:,1],:]
+f3 = Figure(resolution = (800,600))
+ax3 = Axis(f3[1,1,] , xlabel = "x", ylabel = "y", aspect = DataAspect())
+p3 = scatter!(ax3, p_inpoly[:,1], p_inpoly[:,2], color = :red, markersize = 2)
+f3
+c = 0.0
+# dxdy = 1/size(p_inpoly)[1]
+dxdy = 0.01*0.01
+
+for i =1:size(p_inpoly)[1]
+    x = p_inpoly[i,1]
+    y = p_inpoly[i,2]
+    r = (y-c)
+    inertia += r^2*dxdy
+end
+println(inertia)
+answer = 1/3*10^4
+println("result: ", answer)
+println("error: ", (answer-inertia)/answer*100, " %")
+println("result: ", 1/12*10^4)
+
+
